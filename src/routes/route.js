081604a8ffc2f9e8f97ba6b685997478c87397fc
adeshsingh <aadeshsingh1947@gmail.com>
2022-08-08 21:22:21 +0530

    my assign 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _= require('lodash')
 const abc = require('../introduction/intro');
 const lord = require('../logger/logger.js');
 const hellfire = require('../validator/formatter.js')
@@ -12,11 +13,29 @@ router.get('/test-me', function (req, res) {
     lord.welcome()
     hellfire.knight()
     third.printDate()
+    let months =['jan','feb','march', 'april', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec']
+    console.log(_.chunk(months,3));
+
+    const oddnum = [1,3,5,7,9,11,13,15,17,19];
+    console.log(_.tail(oddnum));
+     
+    const mykey=[ ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]
+] 
+console.log(_.fromPairs( mykey ));
+
+const arr1=[1,2,3,4,5]
+const arr2=[3,2,4,1,2]
+const arr3=[1,3,4,5,6]
+const arr4=[1,2,3,5,4]
+const arr5=[6,3,2,4,5]
+console.log(_.union(arr1,arr2,arr3,arr4,arr5));
+
 });
 
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
+
 })
 
 router.get('/give-me-students-data',function(req, res){
@@ -24,3 +43,4 @@ router.get('/give-me-students-data',function(req, res){
 })
 module.exports = router;
 // adding this comment for no reason
+
--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -1,7 +1,7 @@
 function knight(){
     let str1 = "      ADESH SINGH      "
-    let str2 = "ADESH SINGH  "
-    let str3 = "adesh singh "
+    let str2 = "adesh singh"  
+    let str3 = "ADESH SINGH"
     console.log(str1.trim())
     console.log(str2.toUpperCase())
     console.log(str3.toLowerCase())
